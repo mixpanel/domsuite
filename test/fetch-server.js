498d3263b6e09e53ca85819bfabd41653bcafea1
teddddd <ted@mixpanel.com>
2018-11-01 17:48:09 -0700

    an actual FetchServer test

--- test/fetch-server.js
+++ test/fetch-server.js
@@ -1,5 +1,14 @@
+import {FetchServer} from '../lib/fetch-server';
+
 describe(`FetchServer`, function() {
-  it(`runs this test`, function() {
-    expect(true).to.be.ok;
+  it(`supports static return values`, async function() {
+    const fetchServer = new FetchServer({
+      '/foo.json': `bar`,
+    });
+    fetchServer.start();
+
+    const res = await fetch(`https://myserver/foo.json`);
+    const val = await res.json();
+    expect(val).to.equal(`bar`);
   });
 });

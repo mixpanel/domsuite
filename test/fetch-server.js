d533d8ab76c2469ffa2c28e6f09e655eb34abb68
Hans Li <hans.li@mixpanel.com>
2022-06-03 22:07:28 +0000

    Restructured the lib to support both cjs and mjs

--- test/fetch-server.js
+++ test/fetch-server.js
@@ -1,15 +1,18 @@
-import {expect} from '@esm-bundle/chai';
-import {FetchServer} from '../lib/fetch-server';
+import { expect } from "@esm-bundle/chai";
+import { FetchServer } from "../esm/index.mjs";
 
 describe(`FetchServer`, () => {
   it(`runs this test`, async () => {
-    const fetchServer = new FetchServer({
-      '/foo.json': {foo: `bar`},
-    }, {debug: false});
+    const fetchServer = new FetchServer(
+      {
+        "/foo.json": { foo: `bar` },
+      },
+      { debug: false }
+    );
     fetchServer.start();
     const res = await fetch(`/foo.json`);
     const val = await res.json();
-    expect(val).to.eql({foo: `bar`});
+    expect(val).to.eql({ foo: `bar` });
     fetchServer.restore();
   });
 });
